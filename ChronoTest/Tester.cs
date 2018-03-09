34800c0ab26262e1bcd6319da21673f239fc6ec1
emmauss <emmausssss@gmail.com>
2018-03-09 15:17:56 +0200

    Add files via upload

---
+++ ChronoTest/Tester.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChronoProfiler;
+using System.Threading;
+
+namespace ChronoTest
+{
+    public static class Tester
+    {
+        public static void Go()
+        {
+            Chrono.Start("Batch procedure");
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Chrono.Start("Batch procedure;Cycle");
+                Thread.Sleep(100);
+                for (int j = 0; j < 10; j++)
+                {
+                    Chrono.Start("Batch procedure;Cycle;Inner Cycle");
+                    
+                    Thread.Sleep(10);
+                    
+                    Chrono.Start("Batch procedure;Cycle;Inner Cycle;Step 1");
+                    Thread.Sleep(10);
+                    Chrono.Stop("Batch procedure;Cycle;Inner Cycle;Step 1");
+
+                    Chrono.Start("Batch procedure;Cycle;Inner Cycle;Step 2");
+                    Thread.Sleep(11);
+                    Chrono.Stop("Batch procedure;Cycle;Inner Cycle;Step 2");
+
+                    Chrono.Start("Batch procedure;Cycle;Inner Cycle;Step 3");
+                    Thread.Sleep(12);
+                    Chrono.Stop("Batch procedure;Cycle;Inner Cycle;Step 3");
+                    
+                    Chrono.Stop("Batch procedure;Cycle;Inner Cycle");
+                }
+                Chrono.Stop("Batch procedure;Cycle");
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Chrono.Start("Batch procedure;Cycle2");
+                Thread.Sleep(100);
+                for (int j = 0; j < 10; j++)
+                {
+                    Chrono.Start("Batch procedure;Cycle2;Inner Cycle");
+                    Thread.Sleep(10);
+                    Chrono.Stop("Batch procedure;Cycle2;Inner Cycle");
+                }
+                Chrono.Stop("Batch procedure;Cycle2");
+            }
+
+            Chrono.Stop("Batch procedure");
+
+            Chrono.ShowResults();
+        }
+    }
+}
